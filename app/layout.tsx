bdb60f684a58feced2e8d1ab8108a3543baf7923
Iqbal-Maulana67 <fikri.rid@gmail.com>
2026-02-13 09:17:29 +0700

    Changing v2 as main routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import BodyBackground from "@/components/v2/body/BodyBackground";
+import Navbar from "@/components/v2/nav/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +36,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.className} antialiased`}
       >
+        <BodyBackground/>
+        <Navbar />
         <Providers>
           {children}
         </Providers>
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,13 @@
-import FloatingEmojis from "@/components/FloatingEmojis";
-import AboutPage from "@/app/_pages/AboutPage";
-import ContactPage from "@/app/_pages/ContactPage";
-import Homepage from "@/app/_pages/HomePage";
-import ProjectPage from "@/app/_pages/ProjectPage";
-import SkillPage from "@/app/_pages/SkillPage";
-import SmoothScroll from "@/components/SmoothScrolling";
+import ContactPageV2 from "@/app/v2/_pages/ContactPage";
+import HomePageV2 from "@/app/v2/_pages/HomePage";
+import TechPage from "@/app/v2/_pages/TechPage";
 
-export default function Home() {
+export default function HomeV2() {
   return (
-    <main className="relative" id="main-page">
-      <FloatingEmojis />
-
-      <Homepage />
-      <ProjectPage />
-      <section className="h-fit w-full flex flex-col lg:flex-row mt-5 p-5 justify-start">
-        <div className="lg:w-1/3 w-full">
-          <AboutPage />
-          <ContactPage />
-        </div>
-        <hr className="my-5 border-gray-700 hidden md:block" />
-        <div className="lg:w-2/3 hidden lg:block md:block">
-          <SkillPage />
-        </div>
-      </section>
-      <section
-        className="h-auto w-full text-gray-500 p-3 text-center"
-        style={{ backgroundColor: "#111" }}
-      >
-        <span>@imfiekri</span>
-      </section>
+    <main>
+      <HomePageV2 />
+      <TechPage />
+      <ContactPageV2 />
     </main>
   );
 }
