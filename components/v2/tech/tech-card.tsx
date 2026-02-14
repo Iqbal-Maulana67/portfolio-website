0fb2f21954a530872c102f91c2e429f76594fc8f
Iqbal-Maulana67 <fikri.rid@gmail.com>
2026-02-14 09:34:22 +0700

    Adding Animation

--- components/v2/tech/tech-card.tsx
+++ components/v2/tech/tech-card.tsx
@@ -1,4 +1,6 @@
-import Image from "next/image";
+'use client';
+
+import Image from 'next/image';
 
 interface TechCardProps {
   dict: any;
@@ -6,9 +8,21 @@ interface TechCardProps {
 
 const TechCard = ({ dict }: TechCardProps) => {
   return (
-    <div className="glass rounded-2xl min-w-sm min-h-full transition-colors px-4 py-4">
+    <div id="tech-card"
+      className={`glass rounded-2xl min-w-sm min-h-full transition-colors px-4 py-4 ${dict.className}`}
+    >
+      <style jsx>{`
+        .glass:hover {
+          border: 1px solid ${dict.color};
+        }
+          
+        .glass:after:hover {
+          filter: blur(40px);
+          z-index: -2;
+        }
+      `}</style>
       <div className={`flex rounded items-center gap-4 mb-4`}>
-        <div className={`rounded-full p-2 text-center ${dict.className}`}>
+        <div className={`rounded-full p-2 text-center ${dict.iconClassName}`}>
           <Image src={dict.image} alt="" width={25} height={10} />
         </div>
         <span className="font-semibold">{dict.headerName}</span>
@@ -17,7 +31,10 @@ const TechCard = ({ dict }: TechCardProps) => {
         {dict.list.map((data: string, index: number) => {
           return (
             <div className="flex items-center gap-3 mb-4" key={index}>
-              <div className={`w-2 h-2 rounded-full`} style={{ backgroundColor: dict.color }}></div>
+              <div
+                className={`w-2 h-2 rounded-full`}
+                style={{ backgroundColor: dict.color }}
+              ></div>
               <span>{data}</span>
             </div>
           );
