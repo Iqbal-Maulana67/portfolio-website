7c0d344e43d4db3d4a6c8d1cb35eadd7ddf739ac
Iqbal-Maulana67 <fikri.rid@gmail.com>
2026-08-07 09:31:23 +0800

    Update TopBar responsive style for mobile view

--- app/v2/_pages/HomePage.tsx
+++ app/v2/_pages/HomePage.tsx
@@ -27,7 +27,7 @@ export default function HomePageV2() {
             />{" "}
             Apps
           </h1>
-          <p className="text-xl text-gray-400 max-w-3xl mx-auto mb-10">
+          <p className="text-xl text-gray-400 max-w-3xl mx-auto mb-10 ">
             I’m a full-stack developer who builds scalable, production-ready
             systems with clean architecture, well-designed APIs, and a strong
             focus on quality, ownership, and long-term main
--- components/v2/nav/navbar.tsx
+++ components/v2/nav/navbar.tsx
@@ -1,37 +1,43 @@
-"use client"
+'use client';
 
-import { useEffect, useState } from "react"
-import Link from "next/link"
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Menu, X } from 'lucide-react';
 
 const navItems = [
-  { href: "/", label: "Home" },
-  { href: "/about", label: "About" },
-  { href: "/projects", label: "Project" },
-  { href: "/contact", label: "Contact" },
-]
+  { href: '/', label: 'Home' },
+  { href: '/about', label: 'About' },
+  { href: '/projects', label: 'Project' },
+  { href: '/contact', label: 'Contact' },
+];
 
 export default function Navbar() {
-  const [scrolled, setScrolled] = useState(false)
+  const [scrolled, setScrolled] = useState(false);
+  const [isTopBarCollapsed, setTopbarCollapsed] = useState(false);
 
   useEffect(() => {
     const onScroll = () => {
-      setScrolled(window.scrollY > 40)
-    }
+      setScrolled(window.scrollY > 40);
+    };
 
-    window.addEventListener("scroll", onScroll)
-    return () => window.removeEventListener("scroll", onScroll)
-  }, [])
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
 
   return (
     <div
       className={`
-        fixed top-0 left-0 right-0 z-50 transition-all duration-300
-        ${scrolled 
-          ? "bg-gray-900/30 backdrop-blur-md shadow-lg py-2" 
-          : "bg-transparent py-4"}
+        fixed top-0 left-0 right-0 z-50 
+        
       `}
     >
-      <div className="flex justify-between items-center px-10">
+      <div
+        className={`flex justify-between items-center px-5 md:px-10 py-2 transition-all duration-300 ${
+          scrolled
+            ? 'bg-gray-900/30 backdrop-blur-md shadow-lg py-2'
+            : 'bg-transparent py-4'
+        }`}
+      >
         <Link href="/" className="text-lg font-bold">
           IM.FIEKRI
         </Link>
@@ -54,7 +60,45 @@ export default function Navbar() {
             Resume
           </Link>
         </div>
+        <div className="items-center gap-1 flex md:hidden">
+          <div
+            className="px-2 py-2 rounded-full bg-gradient-to-r from-blue-500 to-purple-500 hover:from-cyan-500 hover:to-purple-600 text-white text-sm font-medium transition"
+            onClick={() => {
+              setTopbarCollapsed(!isTopBarCollapsed);
+            }}
+          >
+            {isTopBarCollapsed ? <X></X> : <Menu></Menu>}
+          </div>
+        </div>
+      </div>
+      <div
+        className={`w-full bg-gray-900/30 backdrop-blur-md shadow-lg overflow-hidden transition-all duration-300 ease-in-out ${
+          isTopBarCollapsed
+            ? 'max-h-96 opacity-100 py-2'
+            : 'max-h-0 opacity-0 py-0'
+        }`}
+      >
+        <div className="flex flex-col">
+          {navItems.map((item, index) => (
+            <div
+              key={item.href}
+              style={{ transitionDelay: `${index * 50}ms` }}
+              className={`py-4 border-t border-t-white text-sm font-medium transition-all duration-300 ease-out ${
+                isTopBarCollapsed
+                  ? 'opacity-100 translate-y-0'
+                  : 'opacity-0 -translate-y-4'
+              }`}
+            >
+              <Link
+                href={item.href}
+                className="px-4 py-2 rounded-full text-sm font-medium text-white/80 transition"
+              >
+                {item.label}
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  )
+  );
 }
